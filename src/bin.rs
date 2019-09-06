4edd0ffff0d62d060c4f8877fd09961363d4b179
Robin Appelman <robin@icewind.nl>
2019-09-06 12:15:13 +0200

    reconnect after errors

--- src/bin.rs
+++ src/bin.rs
@@ -1,13 +1,27 @@
 use mhz19::MHZ19;
 use std::time::Duration;
 use std::thread::sleep;
+use std::ffi::OsStr;
+
+const DELAY: Duration = Duration::from_secs(1);
 
 fn main() {
-    let mut mhz19 = MHZ19::open("/dev/ttyUSB0").unwrap();
-    let delay = Duration::from_secs(1);
     loop {
-        let value = mhz19.read().unwrap();
-        println!("{}", value);
-        sleep(delay);
+        listen("/dev/ttyUSB0");
+        sleep(DELAY);
+    }
+}
+
+fn listen<T: AsRef<OsStr> + ?Sized>(port: &T) {
+    let mut mhz19 = MHZ19::open(port).unwrap();
+    loop {
+        match { mhz19.read()} {
+            Ok(value) => println!("{}", value),
+            Err(err) => {
+                eprintln!("Error while reading value: {}, reconnecting", err);
+                return;
+            }
+        };
+        sleep(DELAY);
     }
 }
