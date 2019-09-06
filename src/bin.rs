630087ac72b91d6885e7089e10e5d6ee7f6d1981
Robin Appelman <robin@icewind.nl>
2019-09-06 21:55:54 +0200

    take tty path as arugment

--- src/bin.rs
+++ src/bin.rs
@@ -2,20 +2,24 @@ use mhz19::MHZ19;
 use std::time::Duration;
 use std::thread::sleep;
 use std::ffi::OsStr;
+use std::env;
 
 const DELAY: Duration = Duration::from_secs(1);
 
 fn main() {
-    loop {
-        listen("/dev/ttyUSB0");
-        sleep(DELAY);
+    match env::args().skip(1).next() {
+        Some(path) => loop {
+            listen(&path);
+            sleep(DELAY);
+        },
+        None => eprintln!("Path required")
     }
 }
 
 fn listen<T: AsRef<OsStr> + ?Sized>(port: &T) {
     let mut mhz19 = MHZ19::open(port).unwrap();
     loop {
-        match { mhz19.read()} {
+        match { mhz19.read() } {
             Ok(value) => println!("{}", value),
             Err(err) => {
                 eprintln!("Error while reading value: {}, reconnecting", err);
